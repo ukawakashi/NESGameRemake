f9c99bef2f0db238e8c0a7bd0a7d43a6ac7c3ffa
PTChinh <PTChinh@users.noreply.github.com>
2019-07-11 21:59:23 +0700

    Edit Entity.h and Entity.cpp

--- NESGame/NESGame/GameObjects/Entity.cpp
+++ NESGame/NESGame/GameObjects/Entity.cpp
@@ -3,29 +3,30 @@
 
 Entity::Entity()
 {
-    Tag = None;
+	Tag = None;
 }
 
 D3DXVECTOR3 Entity::GetPosition()
 {
-    return D3DXVECTOR3(posX, posY, 0);
+	return D3DXVECTOR3(posX, posY, 0);
 }
 
-RECT Entity::GetBound()
+Rect Entity::GetBound()
 {
-    RECT bound;
+	Rect bound;
 
-    bound.left = posX - width / 2;
-    bound.right = posX + width / 2;
-    bound.top = posY - height / 2;
-    bound.bottom = posY + height / 2;
+	bound.left = posX - width / 2;
+	bound.right = posX + width / 2;
+	bound.top = posY - height / 2;
+	bound.bottom = posY + height / 2;
 
-    return bound;
+	return bound;
 }
 
 void Entity::OnCollision(Entity *impactor, CollisionReturn data, Entity::SideCollisions side)
 {
-    vx = 0, vy = 0;
+	vx = 0, vy = 0;
+
 }
 
 void Entity::OnSetPosition(D3DXVECTOR3 pos)
@@ -35,90 +36,115 @@ void Entity::OnSetPosition(D3DXVECTOR3 pos)
 
 void Entity::Update(float dt)
 {
-    //velocity = pixel / s
-    posX += vx * dt;
-    posY += vy * dt;
+	//velocity = pixel / s
+	posX += vx * dt;
+	posY += vy * dt;
 }
 
 void Entity::SetPosition(float x, float y)
 {
-    SetPosition(D3DXVECTOR2(x, y));
+	SetPosition(D3DXVECTOR2(x, y));
 }
 
 void Entity::SetPosition(D3DXVECTOR2 pos)
 {
-    SetPosition(D3DXVECTOR3(pos.x, pos.y, 0));
+	SetPosition(D3DXVECTOR3(pos.x, pos.y, 0));
 }
 
 void Entity::SetPosition(D3DXVECTOR3 pos)
 {
-    this->posX = pos.x;
-    this->posY = pos.y; 
+	this->posX = pos.x;
+	this->posY = pos.y;
 
-    OnSetPosition(pos);
+	OnSetPosition(pos);
 }
 
 void Entity::AddPosition(D3DXVECTOR3 pos)
 {
-    this->SetPosition(this->GetPosition() + pos);
+	this->SetPosition(this->GetPosition() + pos);
 }
 
 void Entity::AddPosition(D3DXVECTOR2 pos)
 {
-    AddPosition(D3DXVECTOR3(pos));
+	AddPosition(D3DXVECTOR3(pos));
 }
 
 void Entity::AddPosition(float x, float y)
 {
-    AddPosition(D3DXVECTOR3(x, y, 0));
+	AddPosition(D3DXVECTOR3(x, y, 0));
 }
 
 void Entity::SetWidth(int width)
 {
-    this->width = width;
+	this->width = width;
 }
 
 int Entity::GetWidth()
 {
-    return width;
+	return width;
 }
 
 void Entity::SetHeight(int height)
 {
-    this->height = height;
+	this->height = height;
 }
 
 int Entity::GetHeight()
 {
-    return height;
+	return height;
 }
 
 float Entity::GetVx()
 {
-    return vx;
+	return vx;
 }
 
 void Entity::SetVx(float vx)
 {
-    this->vx = vx;
+	this->vx = vx;
 }
 
 void Entity::AddVx(float vx)
 {
-    this->vx += vx;
+	this->vx += vx;
 }
 
 float Entity::GetVy()
 {
-    return vy;
+	return vy;
 }
 
 void Entity::SetVy(float vy)
 {
-    this->vy = vy;
+	this->vy = vy;
 }
 
 void Entity::AddVy(float vy)
 {
-    this->vy += vy;
+	this->vy += vy;
+}
+
+void Entity::SetCenter(D3DXVECTOR2 center)
+{
+	Center = center;
+}
+D3DXVECTOR2 Entity::GetCenter()
+{
+	if (!Center)
+		return D3DXVECTOR2(posX, posY);
+	return Center;
+}
+
+bool Entity::isDead()
+{
+	if (this->GetPosition().x == 0 && this->GetPosition().y == 0)
+		return true;
+	return false;
+}
+bool Entity::isInCamera(Camera * camera)
+{
+	if ((this->posX >= camera->GetBound().left) && (this->posX <= camera->GetBound().right)
+		&& (this->posY >= camera->GetBound().top) && (this->posY <= camera->GetBound().bottom))
+		return true;
+	return false;
 }
--- NESGame/NESGame/GameObjects/Entity.h
+++ NESGame/NESGame/GameObjects/Entity.h
@@ -3,93 +3,134 @@
 #include <d3dx9.h>
 #include <d3d9.h>
 #include <map>
-
+#include "../GameComponents/ReadResource.h"
+#include "../GameControllers/ResourceManager.h"
+#include "../GameComponents/Camera.h"
 class Entity
 {
 
 public:
 
-    Entity();
+	Entity();
 
-    enum SideCollisions
-    {
-        Left, //0
-        Right, //1
-        Top, //2
-        Bottom, //3
-        TopLeft, //4
-        TopRight, //5
-        BottomLeft, //6
-        BottomRight, //7
-        NotKnow
-    };
+	enum SideCollisions
+	{
+		Left, //0
+		Right, //1
+		Top, //2
+		Bottom, //3
+		TopLeft, //4
+		TopRight, //5
+		BottomLeft, //6
+		BottomRight, //7
+		NotKnow
+	};
 
-    struct CollisionReturn
-    {
-        bool IsCollided;
-        RECT RegionCollision;
-    };
+	struct CollisionReturn
+	{
+		bool IsCollided = false;
+		Rect RegionCollision;
+	};
 
-    enum EntityTypes
-    {
-        None, Brick, Enemy, Mario, Static, BrickGoldNormal, BrickGoldEated
-    };
+	enum EntityTypes
+	{
+		None,
+		PlayerOne,
+		Enemy,
+		CheckPoint,
+		Land,
+		Rope,
+		LandWood,
+		Camel,
+		CamelSpit,
+		AppleObject,
+		Fired,
+		AppleThrow,
+		Bowl,
+		KnifeEnemy3,
+		flagPole,
+		StringHori,
+		BrickDrop,
+		stair1,
+		stair2,
+		Heart,
+		Lamp,
+		stair,
+		box,
+		flare,
+		civilian,
+		jafar,
+		Peddler,
+		Menu,
+		_1Up,
+		RedRock,
+		GenieBonus,
+		AbuBonus,
+		Wall
+	};
 
-    EntityTypes Tag; //Tag de nhan vien loai Entity
+	EntityTypes Tag; //Tag de nhan vien loai Entity
 
-    virtual RECT GetBound();
+	virtual Rect GetBound();
 
-    virtual void SetPosition(float x, float y);
+	virtual void SetPosition(float x, float y);
 
-    virtual void SetPosition(D3DXVECTOR2 pos);
+	virtual void SetPosition(D3DXVECTOR2 pos);
 
-    virtual void SetPosition(D3DXVECTOR3 pos);
+	virtual void SetPosition(D3DXVECTOR3 pos);
 
-    virtual void AddPosition(D3DXVECTOR3 pos);
+	virtual void AddPosition(D3DXVECTOR3 pos);
 
-    virtual void AddPosition(D3DXVECTOR2 pos);
+	virtual void AddPosition(D3DXVECTOR2 pos);
 
-    virtual void AddPosition(float x, float y);
+	virtual void AddPosition(float x, float y);
 
-    virtual void SetWidth(int width);
+	virtual void SetWidth(int width);
 
-    virtual int GetWidth();
+	virtual int GetWidth();
 
-    virtual void SetHeight(int height);
+	virtual void SetHeight(int height);
 
-    virtual int GetHeight();
+	virtual int GetHeight();
 
-    virtual float GetVx();
+	virtual float GetVx();
 
-    virtual void SetVx(float vx);
+	virtual void SetVx(float vx);
 
-    virtual void AddVx(float vx);
+	virtual void AddVx(float vx);
 
-    virtual float GetVy();
+	virtual float GetVy();
 
-    virtual void SetVy(float vy);
+	virtual void SetVy(float vy);
 
-    virtual void AddVy(float vy);
+	virtual void AddVy(float vy);
 
-    virtual D3DXVECTOR3 GetPosition();
+	virtual D3DXVECTOR3 GetPosition();
 
-    virtual void Update(float dt);
+	virtual void Update(float dt);
 
-    //kiem soat viec va cham
-    //khi xay ra va cham voi 1 thuc the nao do thi ham nay se dc goi de xu ly
-    virtual void OnCollision(Entity *impactor, CollisionReturn data, SideCollisions side);
+	//kiem soat viec va cham
+	//khi xay ra va cham voi 1 thuc the nao do thi ham nay se dc goi de xu ly
+	virtual void OnCollision(Entity *impactor, CollisionReturn data, SideCollisions side);
+	virtual void SetCenter(D3DXVECTOR2 center);
+	D3DXVECTOR2 GetCenter();
 
+	//kiem tra nhan vat trong game da chet chua
+	bool isDead();
+	bool isInCamera(Camera* camera);
 protected:
 
-    //duoc goi khi set position cua Entity, dung cho ke thua
-    virtual void OnSetPosition(D3DXVECTOR3 pos);
+	//duoc goi khi set position cua Entity, dung cho ke thua
+	virtual void OnSetPosition(D3DXVECTOR3 pos);
 
-    //vi tri tam position x va y
-    float posX, posY;
+	//vi tri tam position x va y
+	float posX, posY;
 
-    //phan toc vx, vy
-    float vx, vy;
+	//phan toc vx, vy
+	float vx, vy;
 
-    //size cua entity
-    float width, height;
+	//size cua entity
+	float width, height;
+
+	D3DXVECTOR2 Center;
 };
