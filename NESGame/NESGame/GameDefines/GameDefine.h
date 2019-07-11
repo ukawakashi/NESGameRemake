7d58c258f4f995f092fd70250e2ffbf9e731eb02
PTChinh <PTChinh@users.noreply.github.com>
2019-07-11 21:53:32 +0700

    Edit GameDefine.h

--- NESGame/NESGame/GameDefines/GameDefine.h
+++ NESGame/NESGame/GameDefines/GameDefine.h
@@ -1,8 +1,9 @@
 #pragma once
 namespace Define
 {
-    const float PLAYER_MAX_JUMP_VELOCITY = 480.0f; //van toc nhay lon nhat
-    const float PLAYER_MIN_JUMP_VELOCITY = -480.0f; //van toc nhay thap nhat
-    const float PLAYER_MAX_RUNNING_SPEED = 350.0f; //toc do chay nhanh nhat cua player
-    const float PLAYER_BOTTOM_RANGE_FALLING = 6.0f; // do dai va cham voi bottom neu nhu va cham bottom nho hon thi player se bi roi xuong
+	const float PLAYER_MAX_JUMP_VELOCITY = 500.0f; //van toc nhay lon nhat
+	const float PLAYER_MIN_JUMP_VELOCITY = -500.0f; //van toc nhay thap nhat
+	const float PLAYER_MAX_RUNNING_SPEED = 200.0f; //toc do chay nhanh nhat cua player
+	const float PLAYER_BOTTOM_RANGE_FALLING = 8.0f; // do dai va cham voi bottom neu nhu va cham bottom nho hon thi player se bi roi xuong
+
 }
